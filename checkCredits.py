a2db1ac1980397d691274f0abbb4b43657f8ee9a
Nikhil M <mail@nikhilm.in>
2021-05-20 12:35:23 +0000

    use environment variables and notify func

--- checkCredits.py
+++ checkCredits.py
@@ -4,7 +4,22 @@ import os
 
 def check_credits():
     TOKEN=os.environ.get('TOKEN')
-    queryParameters = {'type': 4, 'authkey': TOKEN}
+    MSG_TYPE=os.environ.get('MSG_TYPE')
+    THRESHOLD=os.environ.get('THRESHOLD')
+
+    queryParameters = {'type': MSG_TYPE, 'authkey': TOKEN}
     credits=requests.get('https://control.msg91.com/api/balance.php', params=queryParameters).text
     print(credits)
     
+    if credits > THRESHOLD:
+        notify_slack(credits)
+
+
+def notify_slack(credits):
+    SLACK_URL=os.environ.post('SLACK_URL')
+    payload = {'text':'Hello, World!'}
+    headers = {'Content-type': 'application/json'}
+    requests.post(SLACK_URL, headers=headers, data=payload)
+
+if __name__ == '__main__':
+    check_credits()
